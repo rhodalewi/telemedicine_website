9df89cc57386b8a2eba7108b91e7157759918fdb
Alewi Rhoda Oyinkansola <alewioyin@gmail.com>
2026-02-17 00:47:10 +0100

    Update backend

--- Backend/src/config/database.js
+++ Backend/src/config/database.js
@@ -8,6 +8,7 @@ const db = mysql.createPool({
     user: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
 
     dateStrings: true
 });
